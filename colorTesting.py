d6f0b045d6d9fa3b2c21ee5deb3162cf4a9bf108
LukeYSA <lukeysa@icloud.com>
2023-02-27 14:09:57 -0800

    Added processed images and code that is used to process

---
+++ colorTesting.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+import os
+
+"""
+*** IMPORTANT ***
+Change these paths to the absolute paths in your context (pwd)
+"""
+# Path to your /input_img directory
+path = r'/Users/lukeyin/Desktop/CSE455/cvblokusproject/TrainingImages/PieceRecognition'
+# Path to your /output_img directory
+output_dir = r'/Users/lukeyin/Desktop/CSE455/cvblokusproject/TrainingImages/Processed'
+
+"""
+This is a testing program to play around with image processing and colors
+"""
+
+# green mask
+for i in range(1, 33):
+    os.chdir(path)
+    img = cv2.imread(str(i) + '.jpg')
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower_green = np.array([36, 25, 25])
+    upper_green = np.array([86, 255, 255])
+    green_mask = cv2.inRange(hsv, lower_green, upper_green)
+    green_mask = cv2.bitwise_not(green_mask)
+
+    os.chdir(output_dir)
+    cv2.imwrite(str(i) + '_0.jpg', green_mask)
+
+    rotate_temp = green_mask
+    for degree in [90, 180, 270]:
+        rotate = cv2.rotate(rotate_temp, cv2.ROTATE_90_CLOCKWISE)
+        rotate_temp = rotate
+        cv2.imwrite(str(i) + '_' + str(degree) + '.jpg', rotate)
